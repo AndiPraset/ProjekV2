8518ea088971c4dbd6870964b8a8d146ec8951b6
AndiPraset <andiprasetyo2005@gmail.com>
2021-10-08 07:41:53 +0700

    membuat konesi.js

---
+++ koneksi.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'projek'
+})
+
+conn.connect((err) => {
+    if(err)throw err;
+    console.log('mysql terkoneksi');
+});
+
+module.exports
